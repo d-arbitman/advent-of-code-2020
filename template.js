dc68b0535f678a7f79ce7d07691dbd5b043cd0d6
David Arbitman <david@arbitman.net>
2020-12-03 06:16:28 -0600

    day 03

---
+++ template.js
@@ -0,0 +1,16 @@
+const readline = require('readline');
+const fs = require('fs')
+let input = [];
+
+const readInterface = readline.createInterface({
+    input: fs.createReadStream('./dayXX.input'),
+    /*output: process.stdout,*/
+    console: false
+});
+
+
+readInterface.on('line', (line) => {
+  input.push(line);
+}).on('close', () => {
+
+});
